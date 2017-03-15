5344b3c03510ea7c857761e783dbf630c77e7d30
Rachel Kay Kingswood <rachelkaykingswood@gmail.com>
2017-03-15 10:38:56 -0400

    Assignment 7 Complete

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,11 +1,36 @@
 (function() {
     function SongPlayer() {
+/**
+* @desc object constructor that manages song playback
+* @type {Object Constructor}
+*/        
         var SongPlayer = {};
+/**
+* @desc song container of the currently playing audio file
+* @type {Object}
+*/ 
         var currentSong = null;
+/**
+* @desc Buzz object audio file
+* @type {Object}
+*/
         var currentBuzzObject = null;
-        var setSong = {};
         
-        setSong = function(song){
+/**
+* @function playSong
+* @desc plays current song, sets current song.playing to true
+* @param {Object} song
+*/
+        var playSong = function(song) {
+            currentBuzzObject.play();
+            song.playing = true;
+        };
+/**
+* @function setSong
+* @desc Stops currently playing song and loads new audio file as currentBuzzObject
+* @param {Object} song
+*/   
+        var setSong = function(song){
             if (currentBuzzObject) {
                     currentBuzzObject.stop();
                     currentSong.playing = null;
@@ -18,13 +43,21 @@
         
             currentSong = song;  
         };
-        
+/**
+* @function SongPlayer.pause()
+* @desc pauses current audio file and assigns that song's 'playing' attribute as false.
+* @param {Object} song
+*/
         SongPlayer.pause = function(song) {
             currentBuzzObject.pause();
                 song.playing = false;
         };
-        
-         
+/**
+* @function SongPlayer.play()
+* @desc plays current audio file, pausing previously playing file if needed. Assigns the playing song's "playing" attribute as true. 
+* @param
+* @return
+*/        
         SongPlayer.play = function(song) {
             if (currentSong !== song) {
                 } else if (currentSong === song) {
@@ -33,10 +66,10 @@
                     }
                 }
                 
-        setSong(song);
-        currentBuzzObject.play();
-        song.playing = true;
+            setSong(song);
+            playSong(song);
         };
+        
         return SongPlayer;
         }
 
